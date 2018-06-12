7d088e7f1ad8187be7b05caa288c3ab116c4f0c7
kanu0158 <kanu0158@naver.com>
2018-06-12 16:44:09 +0900

    commit

--- src/ui/PhoneMain.java
+++ src/ui/PhoneMain.java
@@ -1,7 +1,7 @@
 package ui;
 
 import javax.swing.JOptionPane;
-import domain.Phone;
+import domain.*;
 
 /**
  * 0.통화종료 1.집전화기
@@ -12,37 +12,32 @@ enum PhoneButt {
 public class PhoneMain {
 	public static void main(String[] args) {
 		PhoneButt[] buttons = {PhoneButt.EXIT,PhoneButt.TELEPHONE,PhoneButt.CELLPHONE,PhoneButt.SMARTPHONE};
-		//Phone[] phone = new Phone[4];
-		Phone phone = new Phone();
-		//int phoneCount = 0;
+		Phone phone = null;
+		CellPhone cp = null;
 		while (true) {
 			switch ((PhoneButt) JOptionPane.showInputDialog(null, "MAIN PAGE", "SELECT MENU", JOptionPane.QUESTION_MESSAGE,
 					null, buttons, null)) {
 			case EXIT:return;
 			case TELEPHONE:
 				phone = new Phone();
+				//phone.name = JOptionPane.showInputDialog("이름");//이런식의 접근은 정보은닉을 할 수 없으니 지양하자
 				phone.setName(JOptionPane.showInputDialog("이름"));
 				phone.setPhoneNum(JOptionPane.showInputDialog("전화번호"));
-				phone.setBrand();
-				phone.setKind(JOptionPane.showInputDialog("종류"));
 				phone.setCall(JOptionPane.showInputDialog("통화내용"));
 				JOptionPane.showMessageDialog(null, phone.toString());
 				break;
 			case CELLPHONE:
-				phone = new Phone();
-				phone.setName(JOptionPane.showInputDialog("이름"));
-				phone.setPhoneNum(JOptionPane.showInputDialog("전화번호"));
-				phone.setBrand();
-				phone.setKind(JOptionPane.showInputDialog("종류"));
-				phone.setCall(JOptionPane.showInputDialog("통화내용"));
-				JOptionPane.showMessageDialog(null, phone.toString());
+				cp = new CellPhone();
+				cp.setName(JOptionPane.showInputDialog("이름"));
+				cp.setPhoneNum(JOptionPane.showInputDialog("전화번호"));
+				cp.setCall(JOptionPane.showInputDialog("통화내용"));
+				cp.setPortable(true);
+				JOptionPane.showMessageDialog(null, cp.toString());
 				break;
 			case SMARTPHONE:
 				phone = new Phone();
 				phone.setName(JOptionPane.showInputDialog("이름"));
 				phone.setPhoneNum(JOptionPane.showInputDialog("전화번호"));
-				phone.setBrand();
-				phone.setKind(JOptionPane.showInputDialog("종류"));
 				phone.setCall(JOptionPane.showInputDialog("통화내용"));
 				JOptionPane.showMessageDialog(null, phone.toString());
 				break;
